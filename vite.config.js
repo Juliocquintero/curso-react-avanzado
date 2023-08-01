0bc14ffa1d33bcce07d0c7e44f49e504449862dc
Julio Quintero <julio.quintero@codetidy.com.mx>
2023-08-01 17:33:25 -0400

    Ajustes varios

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
+
+const manifestForPlugin = {
+	registerType: 'prompt',
+	manifest: {
+		name: 'Petgram',
+		short_name: 'Petgram',
+		description: 'Aplicación de fotos de animales domésticos.',
+		theme_color: '#0f00ff',
+		background_color: '#fff',
+		icons: [
+			{
+				src: '/src/assets/icon.png',
+				sizes: [96, 128, 192, 256, 384, 512],
+			},
+		],
+		orientation: 'portrait',
+	},
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react(), VitePWA(manifestForPlugin)],
+});
